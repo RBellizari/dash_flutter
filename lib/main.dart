81c7c9b7d778cd2af7ead63a2ba2e5776a374666
Robson Bellizari <robsonbellizari@iMac-de-Robson.local>
2020-03-12 09:09:55 -0300

    testes

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,38 @@ class _MyHomePageState extends State<MyHomePage> {
           BoxShadow(color: Colors.black26, blurRadius: 4),
         ]),
         child: Row(children: <Widget>[
-          Expanded(flex: 3, child: Text('Teste')),
+          Expanded(flex: 3, child:
+          Container(
+      height: 59,
+      color: Theme.of(context).appBarTheme.color,
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Theme.of(context).dividerColor,
+          backgroundImage: NetworkImage(
+            "https://media.licdn.com/dms/image/C5603AQEKYex09zfWlg/profile-displayphoto-shrink_100_100/0?e=1563408000&v=beta&t=5FoN-PaQjLNlPGFqXKW2V4pvUqTPd5QfNNUnovyT5ic",
+          ),
+        ),
+        trailing: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.chat),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    ),
+          ),
           Expanded(flex: 7, child: Text('Teste1')),
         ]),
       ),
