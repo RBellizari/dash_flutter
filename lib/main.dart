aba01c3c29fdc6f00e609ff2133062b4550778c0
Robson Bellizari <robsonbellizari@iMac-de-Robson.local>
2020-03-12 13:56:53 -0300

    grafic

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Troco.Tech',
+      title: 'Troco.Tech1',
       theme: ThemeData.dark(),
       home: HomePage(),
     );
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dashboard/screens/appbar/appbar_page.dart';
+import 'package:flutter_dashboard/screens/charts/charts_page.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
-  const HomePage({Key key, this.title = "Troco.Tech - Dashboard"}) : super(key: key);
+  const HomePage({Key key, this.title = "Troco.Tech - Dashboard"})
+      : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -12,9 +14,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
-    body: 
-    Container(
-      child: AppBarDash(),
-));
+        body: Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: AppBarDash(),
+          ),
+          SizedBox(height: 50.0),
+          Expanded(
+           
+              child: Container(
+            width: 500,
+            height: 100,
+            child: ChartsDash(),
+          )),
+          SizedBox(height: 500.0),
+        ],
+      ),
+    ));
   }
 }
