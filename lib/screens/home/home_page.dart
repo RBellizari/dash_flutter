99e074e861ea843b3464a23e7d02a721653ba04f
Robson Bellizari <robsonbellizari@iMac-de-Robson.local>
2020-04-24 13:38:29 -0300

    menu

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -21,6 +21,48 @@ class _HomePageState extends State<HomePage> {
             Expanded(
                 flex: 1,
                 child: Container(
+                 child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Side menu',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.green,
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage('assets/images/cover.jpg'))),
+          ),
+          ListTile(
+            leading: Icon(Icons.input),
+            title: Text('Welcome'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.verified_user),
+            title: Text('Profile'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.border_color),
+            title: Text('Feedback'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      ),
+    
                   color: Colors.green,
                 )),
             Expanded(
@@ -35,12 +77,6 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.purple,
               ),
             ),
-            Container(
-              child:
-              Column(children: <Widget>[
-                Text("Teste"),
-              ],)
-            )
           ],
         ),
       ),
