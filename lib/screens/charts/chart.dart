489a059955c18badafa35c7b0b04cdc6c1b75fcc
Robson Bellizari <robsonbellizari@iMac-de-Robson.local>
2020-04-25 15:58:43 -0300

    teste chart

--- lib/screens/charts/chart.dart
+++ lib/screens/charts/chart.dart
@@ -4,47 +4,24 @@ import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
 class DonutAutoLabelChart extends StatefulWidget {
+
+  
   _DonutAutoLabelChartState createState() => _DonutAutoLabelChartState();
 }
 
 class _DonutAutoLabelChartState extends State<DonutAutoLabelChart> {
   List<charts.Series> seriesList;
 
-  @override
-  Widget build(BuildContext context) {
-    return new charts.PieChart(seriesList,
-
-        // Configure the width of the pie slices to 60px. The remaining space in
-        // the chart will be left as a hole in the center.
-        //
-        // [ArcLabelDecorator] will automatically position the label inside the
-        // arc if the label will fit. If the label will not fit, it will draw
-        // outside of the arc with a leader line. Labels can always display
-        // inside or outside using [LabelPosition].
-        //
-        // Text style for inside / outside can be controlled independently by
-        // setting [insideLabelStyleSpec] and [outsideLabelStyleSpec].
-        //
-        // Example configuring different styles for inside/outside:
-        //       new charts.ArcLabelDecorator(
-        //          insideLabelStyleSpec: new charts.TextStyleSpec(...),
-        //          outsideLabelStyleSpec: new charts.TextStyleSpec(...)),
-        defaultRenderer: new charts.ArcRendererConfig(
-            arcWidth: 60,
-            arcRendererDecorators: [new charts.ArcLabelDecorator()]));
-  }
-
-  /// Create one series with sample hard coded data.
-  static List<charts.Series<LinearSales, int>> _createSampleData() {
+   static List<charts.Series<LinearSales, int>> _createSampleData() {
     final data = [
-      new LinearSales(0, 100),
-      new LinearSales(1, 75),
-      new LinearSales(2, 25),
-      new LinearSales(3, 5),
+       LinearSales(0, 100),
+       LinearSales(1, 75),
+       LinearSales(2, 25),
+       LinearSales(3, 5),
     ];
 
     return [
-      new charts.Series<LinearSales, int>(
+       charts.Series<LinearSales, int>(
         id: 'Sales',
         domainFn: (LinearSales sales, _) => sales.year,
         measureFn: (LinearSales sales, _) => sales.sales,
@@ -54,6 +31,25 @@ class _DonutAutoLabelChartState extends State<DonutAutoLabelChart> {
       )
     ];
   }
+
+  @override
+  void initState(){
+    super.initState();
+    seriesList = _createSampleData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  charts.PieChart(seriesList,
+
+      
+        defaultRenderer:  charts.ArcRendererConfig(
+            arcWidth: 60,
+            arcRendererDecorators: [ charts.ArcLabelDecorator()]));
+  }
+
+  /// Create one series with sample hard coded data.
+ 
 }
 
 /// Sample linear data type.
