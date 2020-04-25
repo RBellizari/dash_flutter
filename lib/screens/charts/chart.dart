3b083ecb54878094499ce7d2b41ee66be6bf2be7
Robson Bellizari <robsonbellizari@iMac-de-Robson.local>
2020-04-25 16:22:11 -0300

    test chart

--- lib/screens/charts/chart.dart
+++ lib/screens/charts/chart.dart
@@ -4,24 +4,22 @@ import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
 class DonutAutoLabelChart extends StatefulWidget {
-
-  
   _DonutAutoLabelChartState createState() => _DonutAutoLabelChartState();
 }
 
 class _DonutAutoLabelChartState extends State<DonutAutoLabelChart> {
   List<charts.Series> seriesList;
 
-   static List<charts.Series<LinearSales, int>> _createSampleData() {
+  static List<charts.Series<LinearSales, int>> _createSampleData() {
     final data = [
-       LinearSales(0, 100),
-       LinearSales(1, 75),
-       LinearSales(2, 25),
-       LinearSales(3, 5),
+      LinearSales(0, 100, Color(0xff3366cc)),
+      LinearSales(1, 75, Color(0xff3366cc)),
+      LinearSales(2, 25, Color(0xff3366cc)),
+      LinearSales(3, 5, Color(0xff3366cc)),
     ];
 
     return [
-       charts.Series<LinearSales, int>(
+      charts.Series<LinearSales, int>(
         id: 'Sales',
         domainFn: (LinearSales sales, _) => sales.year,
         measureFn: (LinearSales sales, _) => sales.sales,
@@ -33,29 +31,27 @@ class _DonutAutoLabelChartState extends State<DonutAutoLabelChart> {
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     seriesList = _createSampleData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return  charts.PieChart(seriesList,
-
-      
-        defaultRenderer:  charts.ArcRendererConfig(
-            arcWidth: 60,
-            arcRendererDecorators: [ charts.ArcLabelDecorator()]));
+    return charts.PieChart(seriesList,
+        defaultRenderer: charts.ArcRendererConfig(
+            arcWidth: 60, arcRendererDecorators: [charts.ArcLabelDecorator()]));
   }
 
   /// Create one series with sample hard coded data.
- 
+
 }
 
 /// Sample linear data type.
 class LinearSales {
   final int year;
   final int sales;
+  final Color colorgraf;
 
-  LinearSales(this.year, this.sales);
+  LinearSales(this.year, this.sales, this.colorgraf);
 }
