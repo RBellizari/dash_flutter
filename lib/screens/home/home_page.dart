7ed7dd96abc2da58e684904d7d422a00c3c57098
Robson Bellizari <robsonbellizari@iMac-de-Robson.local>
2020-04-26 15:59:39 -0300

    container grey

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -71,7 +71,7 @@ class _HomePageState extends State<HomePage> {
               child: Container(
                
                 width: double.infinity,
-                color: Colors.white,
+                color: Colors.grey,
                 child: 
                  SingleChildScrollView(
                  child: Column(
